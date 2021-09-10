4efc4f22e9a176ed2622358cd4379162336a5fb9
Lith <lithiumturkin@gmail.com>
2021-09-11 01:09:03 +0200

    my crop does not work ill ask someone about it

--- src/main/java/lith/block/grapeplant.java
+++ src/main/java/lith/block/grapeplant.java
@@ -1,22 +1,33 @@
 package lith.block;
 
+import lith.registry.grapeseeds;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
-import net.minecraft.block.Material;
-
-import net.minecraft.item.BlockItem;
+import net.minecraft.block.*;
+import net.minecraft.item.AliasedBlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.shape.VoxelShape;
 
-public class grapeplant implements ModInitializer {
-    public static final Block GRAPE_PLANT = new Block(FabricBlockSettings.of(Material.BAMBOO_SAPLING).strength(1.0f));
+import javax.swing.text.html.BlockView;
 
-    @Override
-    public void onInitialize() {
-        Registry.register(Registry.BLOCK, new Identifier("raccoon", "grapeplant"), GRAPE_PLANT);
-        Registry.register(Registry.ITEM, new Identifier("raccoon", "grapeplant"), new BlockItem(GRAPE_PLANT, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+public class grapeplant extends CropBlock {
+    private static final VoxelShape[] AGE_TO_SHAPE = new VoxelShape[]{Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D),
+            Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 3.0D, 16.0D)
+    };
+    public grapeplant(AbstractBlock.Settings settings) {
+        super(settings);
+    }
+
+    public ItemConvertible getSeedsItem() {
+        return grapeseeds.GRAPESEEDS;
+    }
+
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return AGE_TO_SHAPE[(Integer)state.get(this.getAgeProperty())];
     }
 }
